b2e4d36ea55cdadbe4a917685cd360e0f1f12823
chaesuhyeon <ddd8177@gmail.com>
2021-08-07 11:27:47 +0900

    post number

--- board/templatetags/post_filter.py
+++ board/templatetags/post_filter.py
@@ -4,6 +4,10 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+@register.filter
+def sub(value, arg):
+    return value - arg
+
 @register.filter()
 def mark(value):
     extensions = ["nl2br", "fenced_code"]
--- board/views.py
+++ board/views.py
@@ -16,12 +16,14 @@ class PostDetailView(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['post_detail'] = Post.objects.get(id=self.kwargs['pk'])
         context['group'] = Group.objects.get(id = self.kwargs['group_id'])
-
+       
         return context
 
 
+
+
 class PostListView(generic.ListView):
-    paginate_by = 10
+    paginate_by = 5
     context_object_name = 'post_list'
 
     def get_context_data(self, **kwargs):
@@ -42,8 +44,6 @@ class PostListView(generic.ListView):
         # 정렬
 
         if so == 'popular':
-            post_list = Post.objects.filter(bid_id=self.kwargs['pk']).order_by('-post_hit','-create_date')
-        elif so == 'comment':
             post_list = Post.objects.filter(bid_id=self.kwargs['pk']).annotate(num_comment=Count('comment')).order_by('-num_comment','-create_date')
         else:  # recent
             post_list = Post.objects.filter(bid_id=self.kwargs['pk']).order_by('-create_date')
